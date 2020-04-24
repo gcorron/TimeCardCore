69d07400978c5bbb22b4100a271568f9d2cf65a7
Gregory Corron <gcorron@fastmail.com>
2020-04-24 13:09:06 -0700

    finish payment screen

---
+++ TimeCardCore/Controllers/PaymentController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
+using TimeCard.Repo.Repos;
+using TimeCardCore.Models;
+
+namespace TimeCardCore.Controllers
+{
+    public class PaymentController : BaseController
+    {
+        private readonly PaymentRepo _PaymentRepo;
+        private readonly WorkRepo _WorkRepo;
+        private readonly JobRepo _JobRepo;
+        private readonly LookupRepo _LookupRepo;
+
+        public PaymentController(IConfiguration config, IWebHostEnvironment webHostEnvironment, IHttpContextAccessor httpContextAccessor) : base(config, webHostEnvironment, httpContextAccessor)
+        {
+            _PaymentRepo = new PaymentRepo(ConnString);
+            _WorkRepo = new WorkRepo(ConnString);
+            _JobRepo = new JobRepo(ConnString);
+            _LookupRepo = new LookupRepo(ConnString);
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+            var vm = new PaymentViewModel
+            {
+                SelectedContractorId = CurrentUserId,
+                IsAdmin = false,
+                EditPayment = new TimeCard.Domain.Payment { ContractorId = CurrentUserId },
+                PaymentSummary = _PaymentRepo.GetSummary(CurrentUserId),
+                Payments = _PaymentRepo.GetPayments(CurrentUserId)
+            };
+            prepPayment(vm);
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult Index(PaymentViewModel vm, string buttonValue)
+        {
+            switch (buttonValue)
+            {
+                case "Save":
+
+                    if (vm.JobIsTimeCard)
+                    {
+                        if (vm.EditPayment.WorkDay == 0)
+                        {
+                            ModelState.AddModelError("EditPayment.WorkDay", "Please select Work Period");
+                        }
+                    }
+
+                    if (ModelState.IsValid)
+                    {
+                        vm.EditPayment.ContractorId = vm.SelectedContractorId;
+                        vm.EditPayment.JobId = vm.SelectedJobId;
+                        _PaymentRepo.SavePayment(vm.EditPayment);
+                        vm.EditPayment = new TimeCard.Domain.Payment { PayDate = vm.EditPayment.PayDate };
+                        ModelState.Clear();
+                    }
+                    break;
+                case "Delete":
+                    _PaymentRepo.DeletePayment(vm.EditPayment.PayId);
+                    vm.EditPayment = new TimeCard.Domain.Payment { PayDate = vm.EditPayment.PayDate };
+                    ModelState.Clear();
+                    break;
+                case "Summary":
+                    vm.PaymentSummary = _PaymentRepo.GetSummary(vm.SelectedContractorId);
+                    vm.Payments = _PaymentRepo.GetPayments(vm.SelectedContractorId);
+                    prepPayment(vm);
+                    return PartialView("_PaymentSummary", vm);
+                default:
+                    vm.EditPayment = new TimeCard.Domain.Payment { PayDate = vm.EditPayment.PayDate };
+                    ModelState.Clear();
+                    break;
+            }
+            prepPayment(vm);
+            return PartialView("_EditPayment", vm);
+        }
+
+        private void prepPayment(PaymentViewModel vm)
+        {
+            vm.JobIsTimeCard = false;
+            vm.TimeCardsUnpaid = null;
+            if (vm.SelectedJobId != 0 && vm.SelectedContractorId != 0)
+            {
+                var job = _JobRepo.GetJob(vm.SelectedJobId);
+                vm.SelectedJob = job;
+                vm.JobIsTimeCard = (job.BillTypeDescr == "TC");
+                if (vm.JobIsTimeCard)
+                {
+                    vm.TimeCardsUnpaid = _PaymentRepo.GetJobTimeCardUnpaidCycles(vm.SelectedContractorId, vm.SelectedJobId).Select(x => new SelectListItem { Text = x.ToString(), Value = x.WorkDay.ToString() });
+                }
+            }
+            if (vm.Payments == null)
+            {
+                if (vm.SelectedJobId == 0 || vm.SelectedContractorId == 0)
+                {
+                    vm.Payments = Enumerable.Empty<TimeCard.Domain.Payment>();
+                }
+                else
+                {
+                    vm.Payments = _PaymentRepo.GetPaymentsForJob(vm.SelectedContractorId, vm.SelectedJobId);
+                }
+            }
+        }
+    }
+}
---
+++ TimeCardCore/Models/PaymentViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using TimeCard.Domain;
+
+namespace TimeCardCore.Models
+{
+    public class PaymentViewModel
+    {
+        private DateTime BaselineDate = new DateTime(2018, 12, 22);
+        public IEnumerable<PaymentSummary> PaymentSummary { get; set; }
+
+        public bool JobIsTimeCard { get; set; }
+        public int SelectedContractorId { get; set; }
+        public int SelectedJobId { get; set; }
+        public Job SelectedJob { get; set; }
+        public bool CanEdit { get => !(SelectedJobId == 0 || SelectedContractorId == 0); }
+        public bool IsAdmin { get; set; }
+        public Payment EditPayment { get; set; }
+        public IEnumerable<Payment>Payments { get; set; }
+        public IEnumerable<SelectListItem> TimeCardsUnpaid { get; set; }
+        public string WorkDate(decimal workDay)
+        {
+            if (workDay == 0 )
+            {
+                return null;
+            }
+            int cycle = (int)Decimal.Floor(workDay);
+            return $"{BaselineDate.AddDays((double)(cycle * 14 + (workDay - cycle) * 100)): MM/dd/yyyy}";
+        }
+    }
+}
